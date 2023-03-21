e16bdb332eca021799ea1448a59ca0fbb77bed5f
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2023-03-21 18:10:16 +0000

    Fix almost everything

--- app/controllers/sticks_controller.rb
+++ app/controllers/sticks_controller.rb
@@ -3,29 +3,44 @@ class SticksController < ApplicationController
   def index
     puts "in index"
     puts params
-    puts :sticks_round
-    puts @sticks_round
+    puts :new_sticks_round
+    puts @new_sticks_round
   end
 
   def new_game
-    @sticks_round = SticksRound.new
-    @sticks_round.computer_input = 1
-    @sticks_round.ending_sticks_count = 20
+    @new_sticks_round = SticksRound.new
+    @new_sticks_round.computer_input = 1
+    @new_sticks_round.ending_sticks_count = 20
+    @new_sticks_round.starting_sticks_count = 21
     puts "new game"
     render 'sticks/index'
   end
 
   def userinput
     puts "in userinput of sticks"
-    number_of_sticks = params[:sticks_round][:ending_sticks_count].to_i
-    user_input = params[:user_input].to_i
-    sticks_remaining = number_of_sticks - user_input
-    @sticks_round = SticksRound.new
-    @sticks_round.computer_input = calculate_computer_input(sticks_remaining)
-    puts @sticks_round.computer_input
-    @sticks_round.ending_sticks_count = sticks_remaining - @sticks_round.computer_input.to_i
+    puts params
+    @current_sticks_round = SticksRound.new
+    @current_sticks_round.ending_sticks_count = params[:sticks_round][:ending_sticks_count].to_i
+    @current_sticks_round.starting_sticks_count = params[:sticks_round][:starting_sticks_count].to_i
+    @current_sticks_round.computer_input = params[:sticks_round][:computer_input].to_i
+    @current_sticks_round.user_input = params[:user_input].to_i
+
+    if @current_sticks_round.valid?
+      sticks_remaining = @current_sticks_round.starting_sticks_count.to_i - @current_sticks_round.user_input.to_i - @current_sticks_round.computer_input.to_i
+      @new_sticks_round = SticksRound.new
+      @new_sticks_round.computer_input = calculate_computer_input(sticks_remaining)
+      puts @new_sticks_round.computer_input
+      @new_sticks_round.starting_sticks_count = sticks_remaining
+    else
+      puts @current_sticks_round.valid?
+      @new_sticks_round.computer_input = @current_sticks_round.computer_input
+      @new_sticks_round.user_input = ''
+      @new_sticks_round.starting_sticks_count = @current_sticks_round.starting_sticks_count
+    end
+
     # redirect_to controller: 'sticks', action: 'index', sticks_round: @sticks_round
-    redirect_to sticks_index_url, :sticks_round => @sticks_round, var1: 'rr'
+    # redirect_to sticks_index_url, :sticks_round => @sticks_round, var1: 'rr'
+    render :template => 'sticks/index'
   end
 
   def calculate_computer_input(sticks_remaining)
